dbb5090c4c508d4b5d54a77edab96343d49d1dca
Benjamin9999999 <bentechmusili@gmail.com>
2024-06-15 13:14:17 +0300

    Fixed conflicts

--- src/main/java/DataStructures/Main.java
+++ src/main/java/DataStructures/Main.java
@@ -4,14 +4,46 @@ package DataStructures;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] nums = {15, 2, 16, 14, 3, 13, 4, 12, 5, 11, 6, 10, 7, 9, 8, 1,13,14,14};
+        int size = nums.length;
+        int low =0;
+        int high =size-1;
+        int temp =0;
+     quickSort(nums, 0, high);
+     for(int n : nums){
+         System.out.print(n+ ", ");
+     }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+    private static  void quickSort(int [] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
+
         }
     }
+    public static int partition(int [] nums, int low, int high){
+        int pivot = nums[high];
+        int i = low -1;
+//        int temp = 0;
+        for(int j=low; j<high; j++){
+            if((nums[j] < pivot)){
+                i++;
+               int temp= nums[i];
+                nums[i]= nums[j];
+                nums[j]= temp;
+
+            }
+
+        }
+        int temp = nums[high];
+        nums[high]= nums[i+1];
+        nums[i+1] = temp;
+
+    return i+1;
+
+    }
+
+
 }
